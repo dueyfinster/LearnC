58eb548baf469c76b2250c049088cafa5dfe435a
Neil Grogan <neil@grogan.ie>
2013-07-14 00:29:49 +0100

    finished ex7

--- ex7/ex7.c
+++ ex7/ex7.c
@@ -12,4 +12,14 @@ int main(int argc, char *argv[])
 
 	double expected_bugs = bugs * bug_rate;
 	printf("You are expected to have %f bugs.\n", expected_bugs);
+
+	double part_of_universe = expected_bugs / universe_of_defects;
+	printf("That is only a %e portion of the universe.\n", part_of_universe );
+
+	// no sense demo, weird behaviour
+	char nul_byte = '\0';
+	int care_percentage = bugs * nul_byte;
+	printf("Which means you should care %d%%.\n", care_percentage);
+
+	return 0;
 }
